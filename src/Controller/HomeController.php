105b6687f17c7fcb40d55c1fcb48a4ed017b25f5
Robin Lecullier <lecullier.robin@gmail.com>
2023-10-09 19:58:15 +0200

    display games on home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Announcement;
+use App\Repository\AnnouncementRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(AnnouncementRepository $announcementRepository): Response
     {
+        $announcements = $announcementRepository->findAll();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'announcements' => $announcements,
         ]);
     }
 }
